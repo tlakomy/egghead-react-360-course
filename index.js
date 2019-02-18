92af35b4830a94a6059362e1ba185adec183efc6
Tomasz Łakomy <tomasz.lakomy@olx.pl>
2019-02-18 20:08:52 +0100

    Lesson 04 start state

--- index.js
+++ index.js
@@ -3,26 +3,11 @@ import { AppRegistry, StyleSheet, Text, View } from 'react-360';
 
 export default class travelVR extends React.Component {
     render() {
-        const {
-            mainView,
-            menuItem,
-            poland,
-            ukraine,
-            uk,
-            spain,
-            italy,
-            greece,
-            redText
-        } = styles;
+        const { mainView, text } = styles;
 
         return (
             <View style={mainView}>
-                <Text style={[menuItem, poland]}>Poland</Text>
-                <Text style={[menuItem, ukraine]}>Ukraine</Text>
-                <Text style={[menuItem, uk]}>United Kingdom</Text>
-                <Text style={[menuItem, spain]}>Spain</Text>
-                <Text style={[menuItem, italy, redText]}>Italy</Text>
-                <Text style={[menuItem, greece]}>Greece</Text>
+                <Text style={text}>Add an Image here</Text>
             </View>
         );
     }
@@ -31,38 +16,14 @@ export default class travelVR extends React.Component {
 const styles = StyleSheet.create({
     mainView: {
         width: 600,
+        height: 600,
         padding: 10,
         backgroundColor: '#eee',
         alignItems: 'center'
     },
-    menuItem: {
-        fontSize: 40,
-        width: '50%',
-        marginTop: 5,
-        backgroundColor: '#0298D0',
-        color: 'white',
-        textAlign: 'center'
-    },
-    poland: {
-        backgroundColor: '#DC143C'
-    },
-    ukraine: {
-        backgroundColor: '#FFD500'
-    },
-    uk: {
-        backgroundColor: '#00247D'
-    },
-    spain: {
-        backgroundColor: '#C60D1F'
-    },
-    italy: {
-        backgroundColor: '#029246'
-    },
-    greece: {
-        backgroundColor: '#0D5EAF'
-    },
-    redText: {
-        color: 'red'
+    text: {
+        color: 'black',
+        fontSize: 40
     }
 });
 
