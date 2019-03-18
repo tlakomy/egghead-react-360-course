97497a604ced746b51048c2597ef29bb4456d1e3
Tomasz Łakomy <tomasz.lakomy@olx.pl>
2019-03-18 12:01:34 +0100

    Add lesson 06 end state

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 // This file contains the boilerplate to execute your React app.
 // If you want to modify your application's content, start in "index.js"
 
-import { ReactInstance, Location } from 'react-360-web';
+import { ReactInstance, Location, Surface } from 'react-360-web';
 
 function init(bundle, parent, options = {}) {
     const r360 = new ReactInstance(bundle, parent, {
@@ -10,13 +10,34 @@ function init(bundle, parent, options = {}) {
         ...options
     });
 
+    const myCylinderSurface = new Surface(
+        4680,
+        600,
+        Surface.SurfaceShape.Cylinder
+    );
+
+    const myFlatSurface = new Surface(600, 400, Surface.SurfaceShape.Flat);
+    myFlatSurface.setAngle(-Math.PI / 2, 0);
+
     // Render your app content to the default cylinder surface
     r360.renderToSurface(
         r360.createRoot('travelVR', {
             /* initial props */
         }),
-        r360.getDefaultSurface()
+        myCylinderSurface
     );
+
+    r360.renderToSurface(
+        r360.createRoot('Flag', {
+            image: 'flag_italy.png'
+        }),
+        myFlatSurface
+    );
+
+    const location = new Location([3, -1, -1]);
+
+    // r360.renderToLocation(r360.createRoot('Bunny'), r360.getDefaultLocation());
+    r360.renderToLocation(r360.createRoot('Bunny'), location);
 
     // Load the initial environment
     r360.compositor.setBackground(r360.getAssetURL('360_world.jpg'));
