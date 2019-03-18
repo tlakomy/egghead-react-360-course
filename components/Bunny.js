7555c4c7f6b8f69ece0214e43344623376481b2a
Tomasz Łakomy <tomasz.lakomy@olx.pl>
2019-03-18 17:15:11 +0100

    Added lesson 10 end state

--- components/Bunny.js
+++ components/Bunny.js
@@ -1,10 +1,38 @@
 import React from 'react';
-import { AppRegistry, asset, View } from 'react-360';
+import { Animated, asset, View } from 'react-360';
 import Entity from 'Entity';
+import Easing from 'Easing';
 import AmbientLight from 'AmbientLight';
 import PointLight from 'PointLight';
 
+const AnimatedEntity = Animated.createAnimatedComponent(Entity);
+
 export default class Bunny extends React.Component {
+    rotation = new Animated.Value(0);
+    jumpValue = new Animated.Value(0);
+
+    spin() {
+        this.rotation.setValue(0);
+        Animated.timing(this.rotation, {
+            toValue: 360,
+            duration: 4000,
+            easing: Easing.linear
+        }).start(() => this.spin());
+    }
+
+    jump(value) {
+        let currentVal = value === 20 ? 0 : 20;
+        Animated.timing(this.jumpValue, {
+            toValue: currentVal,
+            duration: 500
+        }).start(() => this.jump(currentVal));
+    }
+
+    componentDidMount() {
+        this.spin();
+        this.jump(0);
+    }
+
     render() {
         return (
             <View>
@@ -13,9 +41,16 @@ export default class Bunny extends React.Component {
                     intensity={1}
                     style={{ transform: [{ translate: [0, 4, -1] }] }}
                 />
-                <Entity
+                <AnimatedEntity
                     source={{ gltf2: asset('Bunny.gltf') }}
-                    style={{ transform: [{ scale: 0.01 }, { rotateY: 45 }] }}
+                    style={{
+                        transform: [
+                            { scale: 0.01 },
+                            { rotateY: 45 },
+                            { translateY: this.jumpValue },
+                            { rotateY: this.rotation }
+                        ]
+                    }}
                 />
             </View>
         );
