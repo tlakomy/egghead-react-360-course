256befef51ca9308d2889ad2a3fcb08691df9d28
Tomasz Łakomy <tomasz.lakomy@olx.pl>
2019-04-20 16:03:05 +0200

    Updated lesson 10 start state

--- client.js
+++ client.js
@@ -10,9 +10,9 @@ function init(bundle, parent, options = {}) {
         ...options
     });
 
-    const location = new Location([3, -1, -1]);
+    const location = new Location([0, -1, -1]);
 
-    r360.renderToLocation(r360.createRoot('Bunny'), location);
+    r360.renderToLocation(r360.createRoot('Earth'), location);
 
     // Load the initial environment
     r360.compositor.setBackground(r360.getAssetURL('360_world.jpg'));
